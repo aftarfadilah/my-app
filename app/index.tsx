d3479831597a8757101a963fc182c08f2218eaf6
aftarfadilah <s160421095@student.ubaya.ac.id>
2024-08-27 08:15:12 +0700

    latihan 2

--- app/index.tsx
+++ app/index.tsx
@@ -24,6 +24,8 @@ export default function Index() {
     setMyScore(myScore - 5)
   }
 
+  const [myColors, setMyColors] = useState([255, 0, 0])
+
   return (
     <View
       style={{
@@ -46,9 +48,45 @@ export default function Index() {
           margin: 12
         }}
       >
-        <Button title="+5" onPress={handleAddScore}>+5</Button>
-        <Button title="-5" onPress={handleSubScore}>-5</Button>
+        <Button 
+          onPress={handleAddScore}
+          title="+5"
+        />
+        <Button
+          onPress={handleSubScore}
+          title="-5"
+        />
       </View>
+      {/* Start RGB Box */}
+      <Text>RGB Box</Text>
+      <Text style={{ fontSize:50, backgroundColor:`rgb(${myColors[0]},${myColors[1]},${myColors[2]})` }}>&nbsp;&nbsp;&nbsp;&nbsp;</Text>
+      <View style={{flexDirection: "row", gap: 8, margin: 12}}>
+        <Button
+          onPress={() => setMyColors([myColors[0] + 10, myColors[1], myColors[2]])}
+          title="R+"
+        />
+        <Button
+          onPress={() => setMyColors([myColors[0] - 10, myColors[1], myColors[2]])}
+          title="R-"
+        />
+        <Button
+          onPress={() => setMyColors([myColors[0], myColors[1] + 10, myColors[2]])}
+          title="G+"
+        />
+        <Button
+          onPress={() => setMyColors([myColors[0], myColors[1] - 10, myColors[2]])}
+          title="G-"
+        />
+        <Button
+          onPress={() => setMyColors([myColors[0], myColors[1], myColors[2] + 10])}
+          title="B+"
+        />
+        <Button
+          onPress={() => setMyColors([myColors[0], myColors[1], myColors[2] - 10])}
+          title="B-"
+        />
+      </View>
+      <Text>{myColors[0]}, {myColors[1]}, {myColors[2]}</Text>
     </View>
   );
 }
